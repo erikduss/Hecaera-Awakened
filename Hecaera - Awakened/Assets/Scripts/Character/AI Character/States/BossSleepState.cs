20df4b440f48d2fe7c1302ccc12ef40e4e4a5be3
erikduss <erikdusschooten@gmail.com>
2024-03-19 16:39:20 +0100

    implemented start sleeping animation for boss

--- Hecaera - Awakened/Assets/Scripts/Character/AI Character/States/BossSleepState.cs
+++ Hecaera - Awakened/Assets/Scripts/Character/AI Character/States/BossSleepState.cs
@@ -7,9 +7,15 @@ namespace Erikduss
     [CreateAssetMenu(menuName = "A.I/States/Boss Sleep")]
     public class BossSleepState : AIState
     {
+        public bool setPosition = false;
+        [SerializeField] Vector3 sleepingPosition;
+
         public override AIState Tick(AICharacterManager aiCharacter)
         {
-            return base.Tick(aiCharacter);
+            if(setPosition)
+                aiCharacter.transform.position = sleepingPosition;
+
+            return base.Tick(aiCharacter);        
         }
     }
 }
